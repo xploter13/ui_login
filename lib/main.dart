1dc8e95793955cabfdea0d03d929048a2d3fec22
xploter13 <xploter13@gmail.com.br>
2022-06-15 17:50:09 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      /* routes: <String, WidgetBuilder>{
+        '/entrar': (BuildContext context) => SignupPage(),
+      }, */
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //resizeToAvoidBottomPadding: false,
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                child: const Text('Hello',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
+                child: const Text('World',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                child: const Text('.',
+                    style: TextStyle(
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(240, 255, 0, 0))),
+              )
+            ],
+          ),
+        ),
+        Container(
+            padding: const EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: <Widget>[
+                const TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
+                ),
+                const SizedBox(height: 20.0),
+                const TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 5.0),
+                Container(
+                  alignment: const Alignment(1.0, 0.0),
+                  padding: const EdgeInsets.only(top: 15.0, left: 20.0),
+                  child: const InkWell(
+                    child: Text(
+                      'Esqueci a senha',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                          decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40.0),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.greenAccent,
+                    color: Colors.green,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: const Center(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Container(
+                  height: 40.0,
+                  color: Colors.transparent,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.black,
+                            style: BorderStyle.solid,
+                            width: 1.0),
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Center(
+                          child: ImageIcon(
+                              AssetImage('assets/images/facebook.png')),
+                        ),
+                        SizedBox(width: 10.0),
+                        Center(
+                          child: Text('Entrar com facebook',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat')),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )),
+        const SizedBox(height: 15.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Novo no spotify?',
+              style: TextStyle(fontFamily: 'Montserrat'),
+            ),
+            const SizedBox(width: 5.0),
+            InkWell(
+              onTap: () {
+                Navigator.of(context).pushNamed('/signup');
+              },
+              child: const Text(
+                'Registrar',
+                style: TextStyle(
+                    color: Colors.green,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline),
+              ),
+            )
+          ],
+        )
+      ],
+    ));
+  }
+}
